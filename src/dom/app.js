55a6dab12365def4c01c0ed87e3ec29735e1eea6
Jorge <jorestuard@gmail.com>
2023-08-18 01:12:04 +0200

    Conversión de codigo con try await

--- src/dom/app.js
+++ src/dom/app.js
@@ -4,3 +4,85 @@ export const $toDoList = document.querySelector(".toDoList__list");
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* async function getTasks() {
+    try {
+      const response = await fetch('http://localhost:3000/tasks');
+      const tasks = await response.json();
+      console.log(tasks);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+
+  const addTask = async (title) => {
+    try {
+      const response = await fetch('http://localhost:3000/tasks', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ title, completed: false })
+      });
+      const task = await response.json();
+      console.log(task);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+   */
--- src/events/functions.js
+++ src/events/functions.js
@@ -1,48 +1,42 @@
 import {$toDoInput, $btnAddToDo, $toDoList} from "../dom/app.js"
 
-let contadora = 0
-
-const addToDo = () => {
-  
+const addToDo = async () => {
   const toDoInput = $toDoInput.value;
-  if (toDoInput === "") {
-    return alert("Introduce una tarea");
-  } else {
-    $toDoInput.value = "";
-    const task = {
-      method:"POST",
-      headers: { 
-            "Content-Type": "application/json"
-            },
-      body: JSON.stringify({
-        id:contadora,
-        text: toDoInput
-      })
-    }
-
-    fetch("http://localhost:3000/toDoList",task)
-    .then( (result) => result.json())
-    .then((data) => console.log(data))
-
-   contadora++
+  if (toDoInput === "") return alert("Introduce una tarea");
+  $toDoInput.value = "";
+  try {
+    await fetch("http://localhost:3000/toDoList", {
+      method: "POST",
+      headers: { "Content-Type": "application/json", "Cache-Control": "no-cache" },
+      body: JSON.stringify({ title: toDoInput, description: "", completed: false })
+    });
+  } catch (error) {
+    console.error(error);
+    alert("Ocurrió un error al agregar la tarea");
   }
-  
 }
 
-const showTask = ()=>{
-    fetch("http://localhost:3000/toDoList")
-    .then( (result) => result.json())
-    .then((data) => {
-      data.forEach(element => {
-        $toDoList.innerHTML += `<li>${element.text}<button class="btnDelete">Eliminar</button></li>`
-      });
-      
-    })
-  
+const showTask = async () => {
+  const data = await (await fetch("http://localhost:3000/toDoList")).json();
+  $toDoList.innerHTML = "";
+  data.forEach(task => {
+    const li = document.createElement("li");
+    li.textContent = task.title;
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Eliminar";
+    deleteBtn.addEventListener('click', async () => {
+      await fetch(`http://localhost:3000/toDoList/${task.id}`, { method: "DELETE" });
+    });
+    const btnEdit = document.createElement("button");
+    btnEdit.textContent = "Editar";
+    btnEdit.addEventListener('click', async () => {
+      await fetch(`http://localhost:3000/toDoList/${task.title}`, { method: "PUT" });
+    });
+    li.appendChild(deleteBtn);
+    li.appendChild(btnEdit); 
+    $toDoList.appendChild(li);
+  });
 }
 
-
-
-$btnAddToDo.addEventListener("click", addToDo )
-
-document.addEventListener("DOMContentLoaded",showTask)
+$btnAddToDo.addEventListener("click", addToDo);
+showTask();
