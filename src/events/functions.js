6ebc6dce61bc9139fd9bd9185020e3e72e4f14bc
Jorge <jorestuard@gmail.com>
2023-08-22 12:25:59 +0200

    Actualización funcionalidades

--- src/events/functions.js
+++ src/events/functions.js
@@ -1,8 +1,9 @@
+'use strict'
 import {$toDoInput, $btnAddToDo, $toDoList} from "../dom/app.js"
 
-const API_SERVER = "http://localhost:3000/toDoList/";
+let API_SERVER = "http://localhost:3000/toDoList/";
 
-const addToDo = async (task) => {
+const addToDo = async () => {
   const toDoInput =  $toDoInput.value;
   if (toDoInput === "") return alert("Introduce una tarea");
   $toDoInput.value = "";
@@ -18,41 +19,60 @@ const addToDo = async (task) => {
     alert("Ocurrió un error al agregar la tarea");
   }
   $toDoInput.innerHTML = ""; 
-  showTasks();
-}
-
-/* const deleteTask =  async (id) => {
-  console.log("esto borra")
-
-}; */
-
-function deleteTask(id) {
-  console.log("hola")
+  
 };
-
-
- 
 
 let getTasks = async () => {
   let data = await (await fetch(API_SERVER)).json();
     return data
 }
 
-
-
-let showTasks = async () => {
-  let tasks = await getTasks();
-  tasks.forEach(task => {
-    return  $toDoList.innerHTML += `
-      <li>${task.title}</li>
-      <button onclick="deleteTask()">Eliminar</button>`
-  })
- 
+window.deleteTask = async (id) => {
+  await fetch(`http://localhost:3000/toDoList/${id}`, {method: "DELETE"});
 };
 
 const updateTask = async (id, task) => {
+  try{
+    await fetch(`http://localhost:3000/toDoList/${id}`, {
+      method: "PATCH",
+      headers: {"Content-Type": "application/json", "Cache-Control": "no-cache" },
+      body: JSON.stringify(task)
+    });
+  } catch (error) {
+    console.error(error);
+    alert("Ocurrió un error al editar la tarea");
+  };  
+};
 
-};   
+window.taskCompleted = async (id, completed) => {
+  try{
+    await fetch(`http://localhost:3000/toDoList/${id}`, {
+      method: "PATCH",
+      headers: {"Content-Type": "application/json", "Cache-Control": "no-cache"},
+      body: JSON.stringify({completed})
+    })
+  } catch (error) {
+    console.error(error);
+    alert("Ocurrió un error al completar la tarea")
+  };
+  
+};
+
+
+const showTasks = async () => {
+  let tasks = await (await fetch(API_SERVER)).json();
+  tasks.forEach(task => {
+    $toDoList.innerHTML += `
+    <div style="display:flex;">
+      <input type="checkbox" data-id="${task.id}" ${task.completed ? 'checked' : ''} onchange="taskCompleted(${task.id}, this.checked)">
+      <li>${task.title}</li>
+      <button onclick="deleteTask(${task.id})">Eliminar</button>
+      <button onclick="updateTask(${task.title})">Editar</button>
+    </div>
+    `;
+  });
+
+};
 
 
 $btnAddToDo.addEventListener("click", addToDo);
